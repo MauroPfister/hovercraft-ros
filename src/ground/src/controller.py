3ddd13fa2ba91170fd9f27cc7be1ec8ec0d32475
Mauro Pfister <maurofabio@bluewin.ch>
2019-05-09 08:56:23 +0200

    Converted matlab to python code

--- src/ground/src/controller.py
+++ src/ground/src/controller.py
@@ -27,7 +27,7 @@ class Controller():
 		self.t0 = rospy.Time.now()
 
 	def circle(self, t):
-		""" Definition of circular trajectory of radius R"""
+		""" Definition of circular trajectory of radius R """
 		R = 2.0
 		w = 2.0 * np.pi / 10.0
 		c_wt = np.cos(w*t)
@@ -40,22 +40,32 @@ class Controller():
 		return pos, vel, acc, jerk
 
 
-
-
 	def iteration(self):
-		""" One iteration of the control loop """
+		""" One iteration of the control loop 
+			Code written for python 2.7 and hence dot() is used for matrix multiplications """
 
+		# controller parameters (need tuning)
+		# make them class variables later on when everything works
+		k_e = 0.005
+		k_phi = 0.05 * np.eye(2)
+		k_z = 0.005
+		delta = 0.03 * np.array([0.1, 0.1]) 
+
+		# ----- ADD CODE FOR OPTITRACK FEEDBACK HERE -------------
 		# Inputs from camera room should be:
 		# eta (position in world frame)
 		# nu (velocity in body frame)
 		# nu_d (acceleration in body frame) 
-		t = rospy.Time.now - self.t0
-		p, p_d, p_dd, p_ddd = circle(t.to_sec)
+		t = rospy.Time.now() - self.t0
+
+		# calculate trajectory at current time
+		p, p_d, p_dd, p_ddd = circle(t.to_sec())
 
 		# parameters of the hovercraft
-		m = 0.0583		# mass
+		m = 0.0583		# mass		
 		I_z = 0.00013	# moment of inertia around z axis
 
+		# those coefficients are some wild ass guesses ....
 		d_v = 0.05		# linear damping coefficient
 		d_r = 0.00001	# rotational damping coefficient
 
@@ -64,39 +74,47 @@ class Controller():
 		c_psi = np.cos(eta[2])
 		s_psi = np.sin(eta[2])
 
-		R_psi = np.array([[c_psi, -s_psi], [s_psi, c_psi]])		# rotation matrix
-		S_r = nu[2] * np.array([[0.0, -1.0], [1.0, 0.0]])		# skew matrix
+		R_psi_T = np.array([[c_psi, s_psi], [-s_psi, c_psi]])		# transposed rotation matrix
+		S_r = nu[2] * np.array([[0.0, -1.0], [1.0, 0.0]])			# skew matrix
 		B = np.array([[1.0, m * delta[1]], [0.0, m * delta[0]]])
+		B_inv = np.linalg.inv(B)
 		B_d = B[:, 1]
 
-		e = R_psi.T*(eta[0:2] - p)
-		e_d = -S_r*e + nu[]0:2] - R_psi.T*p_d
+		e = R_psi_T.dot(eta[0:2] - p)
+		e_d = -S_r.dot(e) + nu[0:2] - R_psi_T.dot(p_d)
 
-		z_1 = nu[0:2] - R_psi.T*p_d + ke/m*e
+		z_1 = nu[0:2] - R_psi_T.dot(p_d) + k_e/m*e
+		z_1_d = nu_d[0:2] + S_r.dot(R_psi_T).dot(p_d) - R_psi_T.dot(p_dd) + k_e/m*e_d
 
-		# Need to clean this up
-		z_1_d = nu_d(1:2) + S_r*R_psi'*p_d - R_psi'*p_dd + ke/m*e_d;
+		phi = z_1 - delta
+		phi_d = z_1_d
 
-		phi = z_1 - delta;
-		phi_d = z_1_d;
+		h = -D_v.dot(R_psi_T).dot(p_d) + k_e/m*D_v*e - m*R_psi_T.dot(p_dd) + k_e * z_1 - k_e*k_e/m*e
+		h_d = - D_v.dot( - S_r.dot(R_psi_T).dot(p_d) + R_psi_T.dot(p_dd) ) + k_e/m*D_v.dot(e_d)
+			  - m*( -S_r.dot(R_psi_T).dot(p_dd) + R_psi_T.dot(p_ddd) ) + k_e*z_1_d - k_e*k_e/m*e_d
 
-		h = -D_v*R_psi'*p_d + ke/m*D_v*e - m*R_psi'*p_dd + ke * z_1 - ke*ke/m*e;
-		h_d = -D_v*( -S_r*R_psi'*p_d + R_psi'*p_dd) + ke/m*D_v*e_d ...
-			-m*( -S_r*R_psi'*p_dd + R_psi'*p_ddd) + ke*z_1_d - ke*ke/m*e_d;
+		alpha = - B_inv.dot( h - D_v.dot(delta) + e/m + k_phi.dot(phi)/m)
+		alpha_d = - B_inv.dot(h_d + e_d/m + k_phi.dot(phi_d)/m)
 
-		alpha = -B\( h - D_v*delta + e/m + kphi*phi/m);
-		alpha_d = -B\(h_d + e_d/m + kphi*phi_d/m);
+		z_2 = nu[2] - alpha[1]
 
-		z_2 = nu(3) - alpha(2);
+		# controller output as in reference paper
+		u_1 = alpha[0]
+		u_2 = - m*B_d.dot(phi) + d_r*alpha[0] + I_z*alpha_d[1] - k_z*z_2; 
 
-		u(1,1) = alpha(1);
-		u(2,1) = -m*_'*phi + d_r*alpha(1) + I_z*alpha_d(2) - kz*z_2; 
+		# conversion from forward and differential thrust (u_1, u_2) to each motor input (u_L, u_R)
+		# check this, really not sure!!!!!!!
+		# assumes linear relation ship between motor command and generated force by thruster
+		k = 0.08		# conversion coefficient
+		l = 0.0325		# lever arm of thrusters
 
-
-		self.ctrl.header.stamp = time
-		self.ctrl.point.x = thrust_L
-		self.ctrl.point.y = thrust_R
-		self.ctrl.point.z = lift
+		u_L = 0.5 * (u_1 - u2/l) / k
+		u_R = 0.5 * (u_1 + u2/l) / k
+		
+		self.ctrl.header.stamp = rospy.Time.now()
+		self.ctrl.point.x = u_L
+		self.ctrl.point.y = u_R
+		self.ctrl.point.z = 1.0
 
 		self.ctrl_pub.publish(self.ctrl)
 		
