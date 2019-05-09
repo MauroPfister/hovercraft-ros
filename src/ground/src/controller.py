a1e5fdbd31efb8938dd3921c23f8c65b8db67065
hovercraft <mauro.pfister@epfl.ch>
2019-05-09 12:27:55 +0200

    Start implementation of feedback loop

--- src/ground/src/controller.py
+++ src/ground/src/controller.py
@@ -1,4 +1,4 @@
-#!/us_r/bin/env python
+#!/usr/bin/env python
 
 import sys
 import os
@@ -6,6 +6,7 @@ import math
 import numpy as np
 import rospy
 import csv
+import tf2_ros
 import tf
 from geometry_msgs.msg import PointStamped
 
@@ -15,12 +16,15 @@ class Controller():
 
 	def __init__(self):
 		
-		# initialize the tf listener to obtain data from OptiTrack
+		# initialize the tf2 listener to obtain data from OptiTrack
+		#self.tf_buffer = tf2_ros.Buffer()
+		#self.listener = tf2_ros.TransformListener(self.tf_buffer)
+
 		self.listener = tf.TransformListener()
 
 		# initialize publisher for control outputs
 		# ctrl.point.x = left thrust, ctrl.point.y = right thrust, ctrl.point.z = lift
-		self.ctrl = PointStamped
+		self.ctrl = PointStamped()
 		self.ctrl_pub = rospy.Publisher('controls', PointStamped, queue_size=1)
 		
 		# getting initial time
@@ -40,6 +44,14 @@ class Controller():
 		return pos, vel, acc, jerk
 
 
+	def get_transform(self, target_frame, source_frame):
+		""" This function gets the current transform between target_frame and source_frame """
+
+		self.listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(1.0))
+		trans, rot = self.listener.lookupTransform(target_frame, source_frame, rospy.Time(0))	# get latest transform
+		twist = self.listener.lookupTwist(target_frame, source_frame, rospy.Time(0), rospy.Duration(0.1))
+		return trans, rot, twist
+
 	def iteration(self):
 		""" One iteration of the control loop 
 			Code written for python 2.7 and hence dot() is used for matrix multiplications """
@@ -56,10 +68,23 @@ class Controller():
 		# eta (position in world frame)
 		# nu (velocity in body frame)
 		# nu_d (acceleration in body frame) 
-		t = rospy.Time.now() - self.t0
+		target_frame = "hovercraft"
+		source_frame = "world"
+		trans, rot, twist = self.get_transform(target_frame, source_frame)
+
+		print('trans :', trans)
+		print('rot :', rot)
+		print('twist :', twist)
+
+
+		eta = np.array([1.0, 0.0, 0.5])
+		nu = np.array([1.0, 2.0, 3.0])
+		nu_d = np.zeros(3)
+
+		t = (rospy.Time.now() - self.t0).to_sec()
 
 		# calculate trajectory at current time
-		p, p_d, p_dd, p_ddd = circle(t.to_sec())
+		p, p_d, p_dd, p_ddd = self.circle(t)
 
 		# parameters of the hovercraft
 		m = 0.0583		# mass		
@@ -83,18 +108,27 @@ class Controller():
 		e = R_psi_T.dot(eta[0:2] - p)
 		e_d = -S_r.dot(e) + nu[0:2] - R_psi_T.dot(p_d)
 
+		#print('e :', np.shape(e))
+		#print('e_d :', np.shape(e_d))
+
 		z_1 = nu[0:2] - R_psi_T.dot(p_d) + k_e/m*e
 		z_1_d = nu_d[0:2] + S_r.dot(R_psi_T).dot(p_d) - R_psi_T.dot(p_dd) + k_e/m*e_d
+
+		#print('z_1 :', np.shape(z_1))
+		#print('z_1_d :', np.shape(z_1_d))
 
 		phi = z_1 - delta
 		phi_d = z_1_d
 
-		h = -D_v.dot(R_psi_T).dot(p_d) + k_e/m*D_v*e - m*R_psi_T.dot(p_dd) + k_e * z_1 - k_e*k_e/m*e
-		h_d = - D_v.dot( - S_r.dot(R_psi_T).dot(p_d) + R_psi_T.dot(p_dd) ) + k_e/m*D_v.dot(e_d)
+		h = -D_v.dot(R_psi_T).dot(p_d) + k_e/m*D_v.dot(e) - m*R_psi_T.dot(p_dd) + k_e * z_1 - k_e*k_e/m*e
+		h_d = - D_v.dot( - S_r.dot(R_psi_T).dot(p_d) + R_psi_T.dot(p_dd) ) + k_e/m*D_v.dot(e_d) \
 			  - m*( -S_r.dot(R_psi_T).dot(p_dd) + R_psi_T.dot(p_ddd) ) + k_e*z_1_d - k_e*k_e/m*e_d
 
 		alpha = - B_inv.dot( h - D_v.dot(delta) + e/m + k_phi.dot(phi)/m)
 		alpha_d = - B_inv.dot(h_d + e_d/m + k_phi.dot(phi_d)/m)
+
+		#print('alpha :', np.shape(alpha))
+		#print('alpha_d :', np.shape(alpha_d))
 
 		z_2 = nu[2] - alpha[1]
 
@@ -108,8 +142,8 @@ class Controller():
 		k = 0.08		# conversion coefficient
 		l = 0.0325		# lever arm of thrusters
 
-		u_L = 0.5 * (u_1 - u2/l) / k
-		u_R = 0.5 * (u_1 + u2/l) / k
+		u_L = 0.5 * (u_1 - u_2/l) / k
+		u_R = 0.5 * (u_1 + u_2/l) / k
 		
 		self.ctrl.header.stamp = rospy.Time.now()
 		self.ctrl.point.x = u_L
