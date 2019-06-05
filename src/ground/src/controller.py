ac0e6a2b6a0b1b8d1871394884dac3cb5dadfcc4
Mauro Pfister <maurofabio@bluewin.ch>
2019-06-05 15:11:43 +0200

    Cleanup controller class

--- src/ground/src/controller.py
+++ src/ground/src/controller.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import math
-import numpy as np
 import rospy
-import csv
+
 import tf
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Vector3Stamped
 
+import numpy as np
 
 from dynamic_reconfigure.server import Server
 from ground.cfg import ControlParamsConfig
 
 
 class Controller():
-	""" Trajectory tracking controller for a TinyWhoover hovercraft """
+	"""
+	Trajectory tracking controller for a TinyWhoover hovercraft 
+	"""
 
 	def __init__(self, k_e, k_phi, k_z, delta):
+
+		# frame names
+		self._world_frame = "world"
+		self._body_frame = "hovercraft"
 
 		# parameters of the hovercraft
 		self.m = 0.0583		# mass		
@@ -37,6 +40,7 @@ class Controller():
 		self.k_z = k_z
 		self.delta = delta
 
+		self._armed = True
 		self.lift = 0.5		# lift of hovercraft
 
 		self._listener = tf.TransformListener()
@@ -62,9 +66,10 @@ class Controller():
 		self._p_desired_pub = rospy.Publisher('p_desired', PointStamped, queue_size=1)
 
 
-		# getting initial time
+		# initial time
 		self._t0 = None
 	
+
 	def _param_callback(self, config, level):
 		self.k_e = config['k_e']
 		self.k_phi = config['k_phi'] * np.eye(2)
@@ -75,11 +80,13 @@ class Controller():
 		rospy.loginfo("Update control parameters: k_e = {k_e}, k_phi = {k_phi} ".format(**config))
 		return config
 
+
 	def set_start_time(self):	
 		self._t0 = rospy.Time.now()
 
+
 	def circle(self, t):
-		""" Definition of circular trajectory of radius R """
+		"""Return position, velocity, acceleration and jerk of a circular trajectory. """
 		R = 2.0
 		w = 2.0 * np.pi / 10.0
 		c_wt = np.cos(w*t)
@@ -91,8 +98,9 @@ class Controller():
 
 		return pos, vel, acc, jerk
 
+
 	def line(self, t):
-		""" Definition of linear trajectory """
+		"""Return position, velocity, acceleration and jerk of a linear trajectory. """
 		p0 = np.array([-0.0, -0.0])
 		p1 = np.array([3.0, 2.0])
 		pos = p0*(1 - t/10) + t/10*p1
@@ -108,7 +116,13 @@ class Controller():
 
 		return pos, vel, acc, jerk
 
+
 	def rose_curve(self, t):
+		"""
+		Return position, velocity, acceleration and jerk of rose trajectory.
+		
+		See https://en.wikipedia.org/wiki/Rose_(mathematics) for more information
+		"""
 		k = 3.0/2
 		pos = np.array([np.cos(k*t)*np.cos(t), np.cos(k*t)*np.sin(t)])
 		vel = np.array([-k*np.sin(k*t)*np.cos(t) - np.cos(k*t)*np.sin(t), -k*np.sin(k*t)*np.sin(t) + np.cos(k*t)*np.cos(t)])
@@ -117,38 +131,58 @@ class Controller():
 
 		return pos, vel, acc, jerk
 
-	def _get_transform(self, world_frame, drone_frame):
-		""" This function gets the current position and velocity (expressed in drone_frame)
-			of drone_frame in world_frame"""
-		refpoint = (0,0,0)
-		self._listener.waitForTransform(world_frame, drone_frame, rospy.Time(0), rospy.Duration(1.0))
-		trans, rot = self._listener.lookupTransform(world_frame, drone_frame, rospy.Time(0))	# get latest transform
-		trans_vel, rot_vel = self._listener.lookupTwistFull(drone_frame, world_frame, drone_frame, refpoint
-										, drone_frame, rospy.Time(0), rospy.Duration(0.1))
 
+	def _get_state(self):
+		""" 
+		Return drone state as two numpy arrays.
 
-		# converting velocity from world frame to body frame
-		vel_world = Vector3Stamped()
-		vel_world.header.frame_id = world_frame
-		vel_world.header.stamp = rospy.Time(0)
-		vel_world.vector.x = trans_vel[0]
-		vel_world.vector.y = trans_vel[1]
-		vel_world.vector.z = trans_vel[2]
-		vel_body = self._listener.transformVector3(drone_frame, vel_world)
-		trans_vel = vel_body.vector
+		State is defined as:
+			position expressed in world frame	: eta = (x, y, psi)	
+			velocity expressed in body frame	: nu  = (u, v, r) 
+		"""
+		
+		#self._listener.waitForTransform(world_frame, drone_frame, rospy.Time(0), rospy.Duration(1.0))		# not sure if this is needed
 
-		trans_vel = (vel_body.vector.x, vel_body.vector.y, vel_body.vector.z)
+		# get latest position and rotation
+		pos, rot = self._listener.lookupTransform(self._world_frame, self._drone_frame, rospy.Time(0))
 
-		rot_euler = tf.transformations.euler_from_quaternion(rot, axes='syzx')		# first rotation around y axis of world_frame
+		# get latest linear and angular velocity expressed in world frame
+		lin_vel, ang_vel = self._listener.lookupTwistFull(self._body_frame, self._world_frame, self._body_frame, 
+													(0, 0, 0), self._body_frame, rospy.Time(0), rospy.Duration(0.1))
+		
+		# convert rotation from quaternions to euler
+		# first euler angle is around y axis of world_frame
+		rot_euler = tf.transformations.euler_from_quaternion(rot, axes='syzx')		
 
-		return trans, rot_euler, trans_vel, rot_vel
+		# convert linear velocity from world frame to body frame
+		lin_vel_world = Vector3Stamped()
+		lin_vel_world.header.frame_id = self._world_frame
+		lin_vel_world.vector.x = lin_vel[0]
+		lin_vel_world.vector.y = lin_vel[1]
+		lin_vel_world.vector.z = lin_vel[2]
+		lin_vel_body = self._listener.transformVector3(self._body_frame, lin_vel_world)
+
+		# convert from optitrack coordinates to hovercraft convention
+		# hovercraft_x = optitrack_x, hovercraft_y = optitrack_z, hovercraft_z = - optitrack_y
+		eta = np.array([pos[0], pos[2], - rot_euler[0]])
+		nu = np.array([lin_vel_body.vector.x, lin_vel_body.vector.z, - ang_vel[1]])
+
+		return eta, nu
+
 
 	def _calculate_control(self, k_e, k_phi, k_z, delta, m, I_z, d_v, d_r, eta, nu, nu_d, p, p_d, p_dd, p_ddd):
-		""" Calculation of control output of trajectory tracking controller
-			Reference paper: "Position Tracking for a Nonlinear Underactuated Hovercraft: 
-			Controller Design and Experimental Results" 
+		""" 
+		Calculate control output of trajectory tracking controller.
+
+		Control output is defined as:
+			forward force : u_1
+			steering torque : u_2
+
+		Reference paper: "Position Tracking for a Nonlinear Underactuated Hovercraft: 
+		Controller Design and Experimental Results" 
 			
-			Code written for python 2.7 and hence dot() is used for matrix multiplications """
+		Note: Code written for python 2.7 and hence dot() is used for matrix multiplications 
+		"""
 
 		D_v = np.diag([d_v, d_v])
 
@@ -185,41 +219,15 @@ class Controller():
 
 		return u_1, u_2
 
+
 	def iteration(self):
 		""" One iteration of the control loop """
 
-		# ----- ADD CODE FOR OPTITRACK FEEDBACK HERE -------------
-		# Inputs from camera room should be:
-		# eta (position in world frame)
-		# nu (velocity in body frame)
-		# nu_d (acceleration in body frame) 
-		world_frame = "world"
-		drone_frame = "hovercraft"
-		trans, rot, trans_vel, rot_vel = self._get_transform(world_frame, drone_frame)
+		# get position and velocity vectors
+		eta, nu = self._get_state()
 
-		"""
-		print('--------')
-		print('trans :', trans)
-		print('rot :', 180 / np.pi * np.asarray(rot))
-		print('trans vel :', trans_vel)
-		print('rot vel :', 180 / np.pi * np.asarray(rot_vel))
-		print('--------')
-		"""
-
-		eta = np.array([trans[0], trans[2], -rot[0]])
-		nu = np.array([trans_vel[0], trans_vel[2], -rot_vel[1]])
-		#nu = np.zeros(3)
+		# assume acceleration is zero (could be calculated with finite differences)
 		nu_d = np.zeros(3)
-
-
-
-		"""
-		print('--------')
-		print('eta :', eta[0:2], 180/np.pi*eta[2])
-		print('nu :', nu)
-		print('nu_d :', nu_d)
-		print('--------')
-		"""
 
 		# calculate trajectory at current time
 		t = (rospy.Time.now() - self._t0).to_sec()
@@ -231,13 +239,10 @@ class Controller():
 											eta, nu, nu_d, p, p_d, p_dd, p_ddd)
 
 
-		# conversion from forward and differential thrust (u_1, u_2) to each motor input (u_L, u_R)
-		# check this, really not sure!!!!!!!
-		# assumes linear relation ship between motor command and generated force by thruster
-
+		# convert from forward force and steering moment (u_1, u_2) to motor input (u_L, u_R)
+		# assume linear relation ship between motor command and generated force by thrusters
 		u_L = 0.5 * (u_1 + u_2/self.l) / self.k
 		u_R = 0.5 * (u_1 - u_2/self.l) / self.k
-		#print(u_L, u_R)
 		
 		# publish to topics
 		self._eta.header.stamp = rospy.Time.now()
@@ -268,12 +273,9 @@ class Controller():
 		self._p_desired.point.y = p[1]
 		self._p_desired_pub.publish(self._p_desired)
 
-
 		self._ctrl.header.stamp = rospy.Time.now()
 
-		armed = 1
-
-		if armed:
+		if self._armed:
 			self._ctrl.point.x = u_L
 			self._ctrl.point.y = u_R
 			self._ctrl.point.z = self.lift
@@ -284,10 +286,11 @@ class Controller():
 
 		self._ctrl_pub.publish(self._ctrl)
 		
+
 if __name__ == "__main__":
 	rospy.init_node('trajectory_controller', anonymous=True)
 
-	# control parameters
+	# controller parameters
 	k_e = 0.005
 	k_phi = 0.5 * np.eye(2)
 	k_z = 0.000005
@@ -299,7 +302,7 @@ if __name__ == "__main__":
 	rospy.loginfo('Start trajectory tracking ...')
 	controller.set_start_time()		# set starttime to current time 
 	#controller_freq = rospy.get_param('~controller_freq')
-	rate = rospy.Rate(50)	# 10 Hz
+	rate = rospy.Rate(50)
 
 	while not rospy.is_shutdown():
 		controller.iteration()
