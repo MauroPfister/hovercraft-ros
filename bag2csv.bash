fdcae7d4a55c1d2b260e0ff2ccadcfd47677f83c
hovercraft <mauro.pfister@epfl.ch>
2019-06-19 14:29:21 +0200

    Make bag2csv more userfriendly

--- bag2csv.bash
+++ bag2csv.bash
@@ -1,8 +1,25 @@
+#!/bin/bash
+
+# parse input argument
+if [ "$1" != "" ]; then
+    path="$1"
+else
+    # default path
+    d=`date +%Y%m%d`    # get date as YYYYMMDD
+    path=identification_data/$d
+fi
+
+# create folder if not existent
+if [ ! -d $path ]; then
+    mkdir -p $path;
+fi
+
+# convert bagfiles
 for bagfile in bagfiles/*.bag; do
     echo converting `basename "$bagfile"` to csv ...;
-    n=${bagfile%.*}          # remove the extension `.csv`
+    n=${bagfile%.*}          # remove the extension `.bag`
     n=${n#"${n%_*}_"}     # remove up to the last underscore `_`
     for topic in eta nu controls; do
-        rostopic echo -p -b ${bagfile} $topic > identification_data/${topic}_$n.csv ; 
+        rostopic echo -p -b ${bagfile} $topic > ${path}/${topic}_$n.csv ; 
     done
 done
